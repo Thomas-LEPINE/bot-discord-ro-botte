61ff5e7ddeca321e27133f065690c9b7f4a8ec21
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2021-01-04 23:50:43 +0100

    +

--- commands/fun/exit.js
+++ commands/fun/exit.js
@@ -1,15 +1,12 @@
-const superagent = require("superagent");
-const Discord = require("discord.js");
-
 module.exports.run = async(client, message, args) => {
     
-    message.channel.send('Bah non tu peux pas le stopper connard, t\'as qu\'\à être dev.');
+    message.channel.send('Bah non tu peux pas le stopper, t\'as qu\'\à être dev.');
 };
 
 module.exports.help = {
     name: 'exit', // nom de la commande
     aliases: ['exit', 'stop'], // Tous les mots clés permettant d'executer la commande
-    descritpion: 'aide les débiles, et les remet à leur place', // Description breve de la commande
+    descritpion: 'Aide les débiles, et les remet à leur place', // Description breve de la commande
     args: false, // True si la commande nécessite forcément des arguments (false sinon)
     usage: '', // Message d'usage de la commande (indication quand des arguments sont attendus)
     category: 'fun', // Categorie de la commande (le nom du repertoire dans notre cas)
