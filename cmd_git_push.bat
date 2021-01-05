d49848fc98a95b16d0a78a4a24416a8088e8e811
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2021-01-05 23:16:45 +0100

    test1

--- cmd_git_push.bat
+++ cmd_git_push.bat
@@ -5,9 +5,19 @@ git pull
 git add .
 git status
 set /p commit-message="Message du commit :  "
+@REM set final=
+@REM for %%a in (%commit-message%) do (
+@REM     set final+= %%a
+@REM )
+@REM for %%b in (%final%) do (
+@REM     echo %%b
+@REM )
+@REM @REM for %%a in (%commit-message%) do (
+@REM @REM     echo %%a
+@REM @REM )
 
 git commit -m %commit-message%
-@REM git commit -m %1
+git commit -m %1
 git push
 :: #################
 
