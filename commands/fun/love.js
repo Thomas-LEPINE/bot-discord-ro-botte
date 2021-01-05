ded5ae98fbc7ec5856618b6154a615bd5506faa0
Hugo Remin <reminhugo@gmail.com>
2021-01-05 17:22:41 +0100

    +

--- commands/fun/love.js
+++ commands/fun/love.js
@@ -1,58 +1,61 @@
 module.exports.run = (client, message, args) => {
     // La fonction associée à la commande :
-    var sommep1=0;
-    var sommep2=0;
-    args[0]=args[0].toLowerCase();
-    args[1]=args[1].toLowerCase();
-    for (i = 0; i < args[0].length; i++) {
-        sommep1+=args[0].charCodeAt(i)
-    }
-
-    for (i = 0; i < args[1].length; i++) {
-        sommep2+=args[1].charCodeAt(i)
-    }
-
-    var compat=(sommep1+sommep2)%101;
-    var final_message="";
-
-    if(compat ==0)
+    if(args.length>1)
     {
-        final_message=final_message.concat(final_message,"0%, aucun love ici");
-    }
-    else
-    {
-        if(compat <=25)
+        var sommep1=0;
+        var sommep2=0;
+        args[0]=args[0].toLowerCase();
+        args[1]=args[1].toLowerCase();
+        for (i = 0; i < args[0].length; i++) {
+            sommep1+=args[0].charCodeAt(i)
+        }
+
+        for (i = 0; i < args[1].length; i++) {
+            sommep2+=args[1].charCodeAt(i)
+        }
+
+        var compat=(sommep1+sommep2)%101;
+        var final_message="";
+
+        if(compat ==0)
         {
-            final_message=final_message.concat(final_message,compat.toString(),"%, pas d'espoir entre ces deux là");
+            final_message=final_message.concat(final_message,"0%, aucun love ici");
         }
         else
         {
-            if(compat <=50)
+            if(compat <=25)
             {
-                final_message=final_message.concat(final_message,compat.toString(),"%, ça peut faire un coup d'un soir");
+                final_message=final_message.concat(final_message,compat.toString(),"%, pas d'espoir entre ces deux là");
             }
             else
             {
-                if(compat <=75)
+                if(compat <=50)
                 {
-                    final_message=final_message.concat(final_message,compat.toString(),"%, le mariage est clairement envisageable");
+                    final_message=final_message.concat(final_message,compat.toString(),"%, ça peut faire un coup d'un soir");
                 }
                 else
                 {
-                    if(compat <=99)
+                    if(compat <=75)
                     {
-                        final_message=final_message.concat(final_message,compat.toString(),"%, commencez à réfléchir aux enfants");
+                        final_message=final_message.concat(final_message,compat.toString(),"%, le mariage est clairement envisageable");
                     }
                     else
                     {
-                        final_message=final_message.concat(final_message,"100%, c'est le véritable amour !!!!");
+                        if(compat <=99)
+                        {
+                            final_message=final_message.concat(final_message,compat.toString(),"%, commencez à réfléchir aux enfants");
+                        }
+                        else
+                        {
+                            final_message=final_message.concat(final_message,"100%, c'est le véritable amour !!!!");
+                        }
                     }
                 }
             }
-        }
-    }    
+        }    
 
-    message.channel.send(final_message);
+        message.channel.send(final_message);
+}
 
 };
 
