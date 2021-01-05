e9918685cd72378918aab20ea9242d2089c70669
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2021-01-05 23:19:57 +0100

    modification cmd bat pour automatisation des pull -> commit ->  push

--- cmd_git_push.bat
+++ cmd_git_push.bat
@@ -5,21 +5,8 @@ git pull
 git add .
 git status
 set /p commit-message="Message du commit :  "
-@REM set final=
-@REM for %%a in (%commit-message%) do (
-@REM     set final+= %%a
-@REM )
-@REM for %%b in (%final%) do (
-@REM     echo %%b
-@REM )
-@REM @REM for %%a in (%commit-message%) do (
-@REM @REM     echo %%a
-@REM @REM )
-
 git commit -m "%commit-message%"
-git commit -m %1
 git push
 :: #################
-
 
 set /p enter = Appuyez sur une touche pour terminer ...
