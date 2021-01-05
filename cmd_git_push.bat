61037cc1d0d7119c70bdedd7a013c16329747f29
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2021-01-05 19:34:35 +0100

    test7

--- cmd_git_push.bat
+++ cmd_git_push.bat
@@ -1,11 +1,11 @@
 @echo off
 
-set commit-message
 :: Commandes git ↓
 git pull
 git add .
 git status
 set /p commit-message="Message du commit :  "
+
 git commit -m %commit-message%
 @REM git commit -m %1
 git push
