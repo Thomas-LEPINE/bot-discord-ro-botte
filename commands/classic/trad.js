e74973860bc4945e87969f340712f0264dde4c2f
CouronneMael <couronne.mael@gmail.com>
2021-01-06 14:35:26 +0100

    commande trad

--- commands/classic/trad.js
+++ commands/classic/trad.js
@@ -1,6 +1,19 @@
+nst request = require('request');
+const { MessageEmbed } = require("discord.js");
+
 module.exports.run = (client, message, args) => {
     // La fonction associée à la commande :
     message.channel.send('Fonctionnalité en cours d\'implémentation');
+    request('https://translation.googleapis.com' +  args[0] "&tl=" + args[1] + "&dt=t&q=" + args[2], function (error, response, body) {
+        console.error('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+        
+        var json = {
+          'sourceText' : sourceText,
+          'translatedText' : translatedText
+        };
+    });
 };
 
 module.exports.help = { // Toutes les informations de la commande
@@ -11,4 +24,3 @@ module.exports.help = { // Toutes les informations de la commande
     usage: 'langue de traduction, text', // Message d'usage de la commande (indication quand des arguments sont attendus)
     category: 'classic', // Categorie de la commande (le nom du repertoire dans notre cas)
 };
-//https://translation.googleapis.com
