f672c1f606ea814850f405837b0fdba0db4cfdae
CouronneMael <couronne.mael@gmail.com>
2021-01-06 15:46:26 +0100

    commande trad v3

--- commands/classic/trad.js
+++ commands/classic/trad.js
@@ -2,18 +2,7 @@ const request = require('request');
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run = (client, message, args) => {
-    // La fonction associée à la commande :
-    message.channel.send('Fonctionnalité en cours d\'implémentation');
-    request('https://translation.googleapis.com' +  args[0] + "&tl=" + args[1] + "&dt=t&q=" + args[2], function (error, response, body) {
-        console.error('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-        
-        var json = {
-          'sourceText' : sourceText,
-          'translatedText' : translatedText
-        };
-    });
+    message.channel.send('VA BIEN TE FAIRE ENCULER CA MARCHE PAS CETTE MERDE, C\'EST GRATUIT MAIS OBLIGÉ DE METTRE UNE CARTE BANCAIRE');
 };
 
 module.exports.help = { // Toutes les informations de la commande
