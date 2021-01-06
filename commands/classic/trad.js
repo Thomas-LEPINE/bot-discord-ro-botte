55d31a4d646c53ea42858a46095880fcb7c7dcd1
CouronneMael <couronne.mael@gmail.com>
2021-01-06 14:37:24 +0100

    commande trad v2.1

--- commands/classic/trad.js
+++ commands/classic/trad.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports.run = (client, message, args) => {
     // La fonction associée à la commande :
     message.channel.send('Fonctionnalité en cours d\'implémentation');
-    request('https://translation.googleapis.com' +  args[0] "&tl=" + args[1] + "&dt=t&q=" + args[2], function (error, response, body) {
+    request('https://translation.googleapis.com' +  args[0] + "&tl=" + args[1] + "&dt=t&q=" + args[2], function (error, response, body) {
         console.error('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
