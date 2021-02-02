21158c5f3147f29af73deacb3f19a10473313050
Hugo Remin <reminhugo@gmail.com>
2021-02-02 17:58:39 +0100

    Move

--- commands/administration/move.js
+++ commands/administration/move.js
@@ -1,14 +1,58 @@
 module.exports.run = (client, message, args) => {
     try{
-    //const channel = client.channel('800126482046648381');
-    const channel = message.guild.channels.cache.find(channel => channel.id === '800126482046648381');
-    //channel now holds the channel you want to move people into
+        /*
+        
+          */
 
-    const member = client.author;
-    //member now holds the user that you want to move
-    
-    member.setVoiceChannel(channel).then(() => console.log(`Succes`))
-    //move(channel);
+
+         var first = true;
+         client.guilds.cache.get(message.guild.id).channels.cache.forEach(ch => {
+             if (ch.type === 'text'){
+                 ch.messages.fetch({
+                     limit: 100
+                 }).then(messages => {
+                     const msgs = messages.filter(m => m.author.id === '779099054084194415') //messages envoyés par le bot
+                     msgs.forEach(m => {
+                         if(m.content.includes(message.author.id) && first)
+                         {
+                             first = false;
+                             var team = 0;
+                             let reactions = m.reactions.cache.find(emoji => emoji.emoji.name == '✅'); //Collects the reactions of the message, where reaction = :white_check_mark: (✅)
+                             m.reactions.cache.map(async (reaction) => { //Maps out every reaction made on the collected message
+                                 let usersThatReacted = []; //Initiates usersThatReacted as an array
+                                 let reactedUsers = await reaction.users.fetch(); //Fetches the users that reacted with the ✅ on the collected message
+                                 reactedUsers.map((user) => { //Maps out every user that reacted with ✅
+                                     if(user.id != '779099054084194415'){ //if it's not the bot
+                                         usersThatReacted.push(user);
+                                     }
+                                 });
+
+                                 team += 1;
+
+                                var server = message.guild;
+                            
+                                await server.channels.create("Groupe "+team, {type: 'voice',topic: 'Groupes'});                                                    
+                            
+                                usersThatReacted.forEach(user =>{
+                                    message.guild.members.cache.forEach( async member => {
+                                        //guard clause, early return
+                                        if(member.id === user.id){ member.voice.setChannel(message.guild.channels.cache.find(channel => channel.name==="Groupe "+team));}
+                                    });
+                                });
+
+                                 return;
+                             });
+                             return;
+                         }
+                     })
+                 })
+             } else {
+                 return;
+             }
+         })
+         return;
+
+       
     }
     catch(err){
         console.log(err)
